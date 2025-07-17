9db54f55bac1ebbe37bd861f403e5e136744865a
Aakanksha Tiwari <akki2tiwari@gmail.com>
2025-07-18 00:54:43 +0530

    Update app_smart_swachh (5).py

--- app_smart_swachh (5).py
+++ app_smart_swachh (5).py
@@ -37,7 +37,7 @@ if "reports" not in st.session_state:
 # PAGE SETUP
 # ----------------------------
 st.set_page_config(page_title="Smart Swachh", layout="wide")
-st.title("🧹 Smart Swachh - AI-Powered Civic Reporter")
+st.title("🧹 Smart Swachh : App for Bharat")
 
 # ----------------------------
 # TABS: Citizen & Officer Login
